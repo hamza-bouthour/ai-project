790fd69cf64c1e3646c8bd93365916a803cf80c8
Hamza Bouthour <ham-panini@proton.me>
2025-12-16 20:31:14 -0800

    changed directories for railway deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
